53ba0d99b6dc4207de58f0d84f8350713b45d69c
Hoover, Austin <hooveram@ornl.gov>
2022-09-22 15:31:59 -0400

    Modify sim_btf.py

--- scripts/sim_btf.py
+++ scripts/sim_btf.py
@@ -19,7 +19,13 @@ import btfsim.bunch.utils as butils
 # ------------------------------------------------------------------------------
 start = 0  # start node (name or index)
 stop = 'HZ04'  # stop node (name or index)
-switches = dict()
+
+switches = {
+    'overlapping_pmq': True,  # use overlapping PMQ field model
+    'space_charge': True,  # toggle space charge calculation
+    'decorrelate': False,  # decorrelate inital bunch
+    'bunch_monitors': False  # bunch monitor nodes within lattice
+}
 
 # File paths
 script_name = Path(__file__).stem
@@ -28,8 +34,7 @@ timestamp = time.strftime('%y%m%d%H%M%S')
 outdir = os.path.join('data/_output/', datestamp)
 if not os.path.isdir(outdir):
     os.makedirs(outdir)
-
-fio = dict()
+fio = dict() 
 fio['in'] = dict()
 fio['out'] = dict()
 
@@ -48,10 +53,8 @@ xp0 = 0.0  # [rad]
 yp0 = 0.0  # [rad]
 beam_current = 42.0  # current to use in simulation [mA]
 beam_current_input = 42.0  # current specified in input bunch file [mA]
-switches['decorrelate'] = False  # presevere only x-x', y-y', z-z' correlations
 
 # Space charge
-switches['space_charge'] = True
 sclen = 0.01  # max distance between space charge nodes [m]
 gridmult = 6  # grid resolution = 2**gridmult
 n_bunches = 3  # number of bunches to model
@@ -68,30 +71,31 @@ sim.dispersion_flag = int(dispersion_flag)
 sim.init_lattice(beamlines=["MEBT1", "MEBT2", "MEBT3"], mstatename=fio['in']['mstate'])
 
 # Add bunch monitor nodes at each of the first four quads.
-for node in sim.lattice.getNodes():
-    if node.getName() in ['MEBT:QH01', 'MEBT:QV02', 'MEBT:QH03', 'MEBT:QV04']:
-        filename = os.path.join(outdir, _base + '_bunch_{}.dat'.format(node.getName()))
-        bunch_monitor_node = diagnostics.BunchMonitorNode(filename=filename)
-        node.addChildNode(bunch_monitor_node, node.ENTRANCE)
+if switches['bunch_monitors']:
+    for node in sim.lattice.getNodes():
+        if node.getName() in ['MEBT:QH01', 'MEBT:QV02', 'MEBT:QH03', 'MEBT:QV04']:
+            filename = os.path.join(outdir, _base + '_bunch_{}.dat'.format(node.getName()))
+            bunch_monitor_node = diagnostics.BunchMonitorNode(filename=filename)
+            node.addChildNode(bunch_monitor_node, node.ENTRANCE)
 for node in sim.lattice.getNodes():
     print(node.getName(), node.getPosition())
     
 # Overlapping nodes: replace quads with analytic model. (Must come after 
 # `lattice.init()` but before `lattice.init_sc_nodes()`.)
-z_step = 0.001
-quad_names = []  # Leaving this empty will replace every quad.
-for j in range(n_pmq):
-    pmq_id = j + 1 + 13
-    qname = 'MEBT:FQ{:02.0f}'.format(pmq_id)
-    quad_names.append(qname)
-if switches['replace_quads']:
-    Replace_Quads_to_OverlappingQuads_Nodes(
-        sim.lattice,
-        z_step, 
-        accSeq_Names=["MEBT3"], 
-        quad_Names=quad_names, 
-        EngeFunctionFactory=btf_quad_func_factory,
-    )
+if switches['overlapping_pmq']:
+    z_step = 0.001
+    quad_names = []  # Leaving this empty will replace every quad.
+    for j in range(n_pmq):
+        pmq_id = j + 1 + 13
+        qname = 'MEBT:FQ{:02.0f}'.format(pmq_id)
+        quad_names.append(qname)
+        Replace_Quads_to_OverlappingQuads_Nodes(
+            sim.lattice,
+            z_step, 
+            accSeq_Names=["MEBT3"], 
+            quad_Names=quad_names, 
+            EngeFunctionFactory=btf_quad_func_factory,
+        )
     
 if switches['space_charge']:
     sim.init_sc_nodes(minlen=sclen, solver='fft', gridmult=gridmult, n_bunches=n_bunches)
