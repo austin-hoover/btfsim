e0a3e905632361b3ea63c2b1bad49a89a89ef439
Hoover, Austin <hooveram@ornl.gov>
2022-09-23 12:05:48 -0400

    Add utility git functions

--- btfsim/util/utils.py
+++ btfsim/util/utils.py
@@ -1,5 +1,7 @@
 import glob
+import hashlib
 import os
+import subprocess
 from collections import OrderedDict
 
 
@@ -34,3 +36,47 @@ def get_latest_file(path, *paths):
     latest_file = max(list_of_files, key=os.path.getctime)
     _, filename = os.path.split(latest_file)
     return filename
+
+
+def git_url():
+    url = None
+    try:
+        url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode("utf-8").strip()
+        if url and url.endswith('.git'):
+            url = url[:-4]
+    except subprocess.CalledProcessError:
+        pass
+    return url
+
+
+def is_git_clean():
+    clean = False
+    try:
+        clean = False if subprocess.check_output(['git', 'status', '--porcelain']) else True
+    except subprocess.CalledProcessError:
+        pass
+    return clean
+
+
+def git_revision_hash():
+    commit = None
+    try:
+        commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode("utf-8").strip()
+    except subprocess.CalledProcessError:
+        pass
+    return str(commit)
+
+
+def file_hash(filename):
+    sha1_hash = hashlib.sha1()
+    with open(filename, "rb") as f:
+        # Read and update hash string value in blocks of 4K
+        for byte_block in iter(lambda: f.read(4096), b""):
+            sha1_hash.update(byte_block)
+    return sha1_hash.hexdigest()
+
+
+def url_style(url, text=None):
+    if text is None:
+        text = url
+    return {'text': text, 'url': url}
