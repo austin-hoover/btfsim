d7e666bcc9b99247f39d0da01a1beeeab17f721b
Hoover, Austin <hooveram@ornl.gov>
2022-09-30 14:28:45 -0400

    Rename script

--- scripts/2022-09-29_MEBT1_sshape/test_diagnostics.py
+++ scripts/2022-09-29_MEBT1_sshape/test_diagnostics.py
@@ -16,6 +16,7 @@ from btfsim.lattice.btf_quad_func_factory import btf_quad_func_factory
 from btfsim.sim.sim import Sim
 from btfsim.util import utils
 import btfsim.bunch.utils as butils
+from btfsim.plot import plot
 
 
 # Setup
@@ -33,14 +34,15 @@ script_name = Path(__file__).stem
 datestamp = time.strftime('%Y-%m-%d')
 timestamp = time.strftime('%y%m%d%H%M%S')
 outdir = os.path.join('data/_output/', datestamp)
-if not os.path.isdir(outdir):
-    os.makedirs(outdir)
+utils.ensure_path_exists(outdir)
+utils.ensure_path_exists(os.path.join(outdir, 'figures'))
     
 # Lattice
 fio['in']['mstate'] = 'data/lattice/TransmissionBS34_04212022.mstate'
+fio['in']['bunch'] = 'data/bunch/realisticLEBT_50mA_42mA_8555k.dat'
 
 # Bunch
-bunch_dec_factor = None
+bunch_dec_factor = 20  # reduce number of macroparticles by this factor
 beam_current = 42.0  # current to use in simulation [mA]
 beam_current_input = 42.0  # current specified in input bunch file [mA]
 
@@ -69,39 +71,72 @@ _base = '{}-{}-{}-{}'.format(timestamp, script_name, start, stop)
 fio['out']['bunch'] = _base + '-bunch-{}.dat'.format(stop)
 fio['out']['history'] = os.path.join(outdir, _base + '-history.dat')
 
-sim = Sim(outdir=outdir)
+tracker_kws = dict()
+
+plotter = plot.Plotter(
+    # path=os.path.join(outdir, 'figures'), 
+    path=outdir,
+    default_fig_kws=None, 
+    default_save_kws=None,
+)
+plotter.add_func(
+    plot.proj2d, 
+    fig_kws=dict(
+        figsize=(4.0, 4.0), 
+        constrained_layout=True,
+    ),
+    save_kws=None, 
+    name='proj2d', 
+    axis=(0, 1),
+    bins='auto',
+    profx=True, profy=True, 
+    prof_kws=dict(alpha=0.7, lw=0.7),
+)
+tracker_kws['plotter'] = plotter
+tracker_kws['plot_norm_coords'] = True
+tracker_kws['plot_scale_emittance'] = True
+
+save_bunch = dict()
+save_bunch['dir'] = outdir
+save_bunch['prefix'] = _base
+for node in ['MEBT:QH01', 'MEBT:QV02', 'MEBT:QH03', 'MEBT:QV04']:
+    save_bunch[node] = os.path.join(outdir, _base + '-bunch-{}.dat'.format(node))
+# tracker_kws['save_bunch'] = save_bunch
+
+# Create simulation.
+sim = Sim(
+    outdir=outdir, 
+    tracker_kws=tracker_kws,
+)
 sim.dispersion_flag = False
 sim.init_lattice(
     beamlines=['MEBT1'], 
     mstatename=fio['in']['mstate'],
 )
-    
-# Dump bunch coordinates at each quad entrance.
-for node in sim.lattice.getNodes():
-    if node.getName() in ['MEBT:QH01', 'MEBT:QV02', 'MEBT:QH03', 'MEBT:QV04']:
-        filename = os.path.join(outdir, _base + '-bunch-{}.dat'.format(node.getName()))
-        bunch_monitor_node = diagnostics.BunchMonitorNode(filename=filename)
-        node.addChildNode(bunch_monitor_node, node.ENTRANCE)
+file = open('nodes.txt', 'w')
+file.write('node position')
 for node in sim.lattice.getNodes():
     print(node.getName(), node.getPosition())
-    
+    file.write('{}'.format(node.getName(), node.getPosition()))
+file.close()
+           
+
 if switches['space_charge']:
     sim.init_sc_nodes(min_dist=sclen, solver='fft', gridmult=gridmult, n_bunches=n_bunches)
     
 sim.init_bunch(
-    gen_type='twiss', 
-    dist='waterbag',
-    n_parts=200000,
+    gen_type='load', 
+    bunch_filename=os.path.join(os.getcwd(), fio['in']['bunch']),
+    bunch_file_format='pyorbit',
 )
-sim.attenuate_bunch(beam_current / beam_current_input)
-if bunch_dec_factor is not None and bunch_dec_factor > 1:
-    sim.decimate_bunch(bunch_dec_factor)
+sim.bunchman.attenuate(beam_current / beam_current_input)
+sim.bunchman.decimate(bunch_dec_factor)
 
 
 # Run simulation
 # ------------------------------------------------------------------------------
 if switches['save_init_bunch']:
-    sim.dump_bunch(os.path.join(outdir, _base + '-bunch-init.dat'))
+    sim.bunchman.dump(os.path.join(outdir, _base + '-bunch-init.dat'))
 
 def process_start_stop_arg(arg):
     return "MEBT:{}".format(arg) if type(arg) is str else arg
